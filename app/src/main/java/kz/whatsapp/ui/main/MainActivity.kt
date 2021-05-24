294dcaef12c05307db527a932015df35fa4cc429
Makhanov Madiyar <makhanovable@gmail.com>
2021-05-24 08:24:43 +0600

    init

---
+++ app/src/main/java/kz/whatsapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package kz.whatsapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kz.whatsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
