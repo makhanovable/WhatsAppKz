a8d2f98f4570c42dd706e177ebaf1ad5182ec4fb
Makhanov Madiyar <makhanovable@gmail.com>
2021-05-24 17:28:11 +0600

    temp

---
+++ app/src/main/java/kz/whatsapp/ui/FriendlyMessageAdapter.kt
@@ -0,0 +1,120 @@
+package kz.whatsapp.ui
+
+import android.graphics.Color
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.*
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.image_message.view.*
+import kotlinx.android.synthetic.main.message.view.*
+import kotlinx.android.synthetic.main.message.view.messengerImageView
+import kotlinx.android.synthetic.main.message.view.messengerTextView
+import kz.whatsapp.R
+import kz.whatsapp.ui.MainActivity.Companion.ANONYMOUS
+import kz.whatsapp.ui.model.FriendlyMessage
+
+// The FirebaseRecyclerAdapter class and options come from the FirebaseUI library
+// See: https://github.com/firebase/FirebaseUI-Android
+class FriendlyMessageAdapter(
+    private val options: FirebaseRecyclerOptions<FriendlyMessage>,
+    private val currentUserName: String?
+) :
+    FirebaseRecyclerAdapter<FriendlyMessage, ViewHolder>(options) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return if (viewType == VIEW_TYPE_TEXT) {
+            val view = inflater.inflate(R.layout.message, parent, false)
+            MessageViewHolder(view)
+        } else {
+            val view = inflater.inflate(R.layout.image_message, parent, false)
+            ImageMessageViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, model: FriendlyMessage) {
+        if (options.snapshots[position].text != null) {
+            (holder as MessageViewHolder).bind(model)
+        } else {
+            (holder as ImageMessageViewHolder).bind(model)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (options.snapshots[position].text != null) VIEW_TYPE_TEXT else VIEW_TYPE_IMAGE
+    }
+
+    inner class MessageViewHolder(private val view: View) : ViewHolder(view) {
+        fun bind(item: FriendlyMessage) {
+            view.messageTextView.text = item.text
+            setTextColor(item.name, view.messageTextView)
+
+            view.messengerTextView.text = if (item.name == null) ANONYMOUS else item.name
+            if (item.photoUrl != null) {
+                loadImageIntoView(view.messengerImageView, item.photoUrl!!)
+            } else {
+                view.messengerImageView.setImageResource(R.drawable.ic_launcher_background)
+            }
+        }
+
+        private fun setTextColor(userName: String?, textView: TextView) {
+            if (userName != ANONYMOUS && currentUserName == userName && userName != null) {
+                textView.setBackgroundResource(R.drawable.rounded_message_blue)
+                textView.setTextColor(Color.WHITE)
+            } else {
+                textView.setBackgroundResource(R.drawable.rounded_message_gray)
+                textView.setTextColor(Color.BLACK)
+            }
+        }
+    }
+
+    inner class ImageMessageViewHolder(private val view: View) :
+        ViewHolder(view) {
+        fun bind(item: FriendlyMessage) {
+            loadImageIntoView(view.messageImageView, item.imageUrl!!)
+
+            view.messengerTextView.text = if (item.name == null) ANONYMOUS else item.name
+            if (item.photoUrl != null) {
+                loadImageIntoView(view.messengerImageView, item.photoUrl!!)
+            } else {
+                view.messengerImageView.setImageResource(R.drawable.ic_launcher_background)
+            }
+        }
+    }
+
+    private fun loadImageIntoView(view: ImageView, url: String) {
+        if (url.startsWith("gs://")) {
+            val storageReference = Firebase.storage.getReferenceFromUrl(url)
+            storageReference.downloadUrl
+                .addOnSuccessListener { uri ->
+                    val downloadUrl = uri.toString()
+                    Picasso.get()
+                        .load(downloadUrl)
+                        .into(view)
+                }
+                .addOnFailureListener { e ->
+                    Log.w(
+                        TAG,
+                        "Getting download url was not successful.",
+                        e
+                    )
+                }
+        } else {
+            Picasso.get().load(url).into(view)
+        }
+    }
+
+    companion object {
+        const val TAG = "MessageAdapter"
+        const val VIEW_TYPE_TEXT = 1
+        const val VIEW_TYPE_IMAGE = 2
+    }
+}
---
+++ app/src/main/java/kz/whatsapp/ui/SignInActivity.kt
@@ -0,0 +1,79 @@
+package kz.whatsapp.ui
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kz.whatsapp.R
+
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var signInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+    private val resultLauncher = registerForActivityResult(StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        sign_in_button.setOnClickListener { signIn() }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        signInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+    }
+
+    private fun signIn() {
+        val signInIntent = signInClient.signInIntent
+        resultLauncher.launch(signInIntent)
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount?) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct?.id)
+        val credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnSuccessListener(this) {
+                // If sign in succeeds the auth state listener will be notified and logic to
+                // handle the signed in user can be handled in the listener.
+                Log.d(TAG, "signInWithCredential:success")
+                startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener(this) { e -> // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithCredential", e)
+                Toast.makeText(
+                    this@SignInActivity, "Authentication failed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    companion object {
+        private const val TAG = "SignInActivity"
+    }
+}
